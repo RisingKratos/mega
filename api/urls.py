ef6637f81e7c8273b5f1c6fab76183b4f42b831a
Muslim <risingkratos@gmail.com>
2016-11-06 10:21:23 +0600

    Last Commit

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^api/(?P<email>\w{0,50})/(?P<password>\w{0,50})$', views.login_api_view, name='login_api_view'),
+    url(r'^api/(?P<code_id>[0-9]+)$', views.gem_api_view, name='gem_api_view'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from main_app.models import Code
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def login_api_view(request, email, password):
     user = authenticate(username=email, password=password)
     if user is not None:
         login(request, user)
+        return JsonResponse({"value" : "true"})
+    return JsonResponse({"value" : "false"})
+
+def gem_api_view(request, code_id):
+    code = get_object_or_404(Code, pk=code_id)
+    if code is not None:
+        code.gems -= 1
+        code.save()
         return JsonResponse({"value" : "true"})
     return JsonResponse({"value" : "false"})
